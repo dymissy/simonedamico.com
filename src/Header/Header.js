f53ef662c90cd2942f23f29c058ada9196ab4b87
Simone D'Amico <simone.damico@spreaker.com>
2021-07-31 19:08:56 +0200

    Improve style

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,45 +1,43 @@
 import "./Header.scss";
 import Icon from "./Icons/Icon";
 
-function Header({ menuItems, socialItems }) {
-    return (
-        <header className="container">
-            <div className="row">
-                <nav className="socials col">
-                    <ul className="nav">
-                        {socialItems.map((item) => (
-                            <li key={item.href}>
-                                <a
-                                    className={`icon ${item.label.toLowerCase()}`}
-                                    href={item.href}
-                                    title={item.label}
-                                    target={item.target}
-                                >
-                                    <Icon name={item.label} />
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <nav className="menu col">
-                    <ul className="nav justify-content-end">
-                        {menuItems.map((item) => (
-                            <li key={item.href}>
-                                <a
-                                    className={item.label.toLowerCase()}
-                                    href={`#${item.href}`}
-                                    title={item.label}
-                                    target={item.target}
-                                >
-                                    {item.label}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
-}
+const Header = ({ menuItems, socialItems }) => (
+    <header className="container">
+        <div className="row">
+            <nav className="socials col-md-5">
+                <ul className="nav">
+                    {socialItems.map((item) => (
+                        <li key={item.href}>
+                            <a
+                                className={`icon ${item.label.toLowerCase()}`}
+                                href={item.href}
+                                title={item.label}
+                                target={item.target}
+                            >
+                                <Icon name={item.label} />
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <nav className="menu col">
+                <ul className="nav justify-content-end">
+                    {menuItems.map((item) => (
+                        <li key={item.href}>
+                            <a
+                                className={item.label.toLowerCase()}
+                                href={`#${item.href}`}
+                                title={item.label}
+                                target={item.target}
+                            >
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    </header>
+);
 
 export default Header;
